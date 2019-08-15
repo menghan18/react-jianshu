6b0937092f6ea765103f69b19c4c1cc3129f195a
menghan18 <menghan18@users.noreply.github.com>
2019-08-15 22:53:10 +0800

    header init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { GlobalStyle } from './style';
+import Header from './common/header/index'
 
 class App extends Component {
   render () {
     return (
-      <div>
+      <>
         <GlobalStyle />
-        hello word
-      </div>
+        <Header/>
+      </>
     );
   }
 }
---
+++ src/common/header/style.js
@@ -0,0 +1,88 @@
+import styled from 'styled-components'
+import logoPic from '../../statics/logo.png'
+
+export const HeaderWrapper = styled.div`
+  position: relative;
+  height:58px;
+  border-bottom:1px solid #f0f0f0;
+`
+
+export const Logo = styled.a`
+  position: absolute;
+  display: block;
+  top: 0;
+  width: 0;
+  width: 100px;
+  height: 58px;
+  background: url(${logoPic});
+  background-size: contain;
+`
+
+export const Nav = styled.div`
+  box-sizing: border-box;
+  padding-right: 70px;
+  width: 960px;
+  height: 100%;
+  margin: 0 auto;
+`
+
+export const NavItem = styled.div`
+  line-height: 56px;
+  padding: 0 15px;
+  font-size: 17px;
+  color:#333;
+  &.left {
+    float: left;
+  }
+  &.right{
+    float: right;
+    color: #969696;
+  }
+  &.active{
+    color: #ea6f5a;
+  }
+`
+
+export const NavSearch = styled.input.attrs({
+  placeholder: '搜索'
+})`
+  margin-top: 9px;
+  margin-left: 20px;
+  padding: 0 20px;
+  width: 160px;
+  height: 38px;
+  box-sizing: border-box;
+  border: none;
+  outline: none;
+  font-size: 14px;
+  border-radius: 19px;
+  background: #eee;
+  &::placeholder{
+    color:#999;
+  }
+`
+
+export const Addition = styled.div`
+  position: absolute;
+  right:0;
+  top: 0;
+  height: 56px;
+`
+
+export const Button = styled.div`
+  float: right;
+  margin-top: 9px;
+  margin-right: 20px;
+  padding: 0 20px;
+  line-height: 38px;
+  font-size: 14px;
+  border-radius: 19px;
+  border: 1px solid #ec6149;
+  &.reg{
+    color: #ec6149;
+  }
+  &.writting{
+    color: #fff;
+    background: #ec6149;
+  }
+`
