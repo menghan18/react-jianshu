e2958e4516c459a9656a63c50f2a18719b7aae46
menghan18 <menghan18@users.noreply.github.com>
2019-08-15 16:39:24 +0800

    project init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { GlobalStyle } from './style';
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <GlobalStyle />
+        hello word
+      </div>
+    );
+  }
+}
+
+export default App;
