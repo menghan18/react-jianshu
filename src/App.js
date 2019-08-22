a3203970e689ba3bb215f1158e9a1692554be934
menghan18 <menghan18@users.noreply.github.com>
2019-08-22 19:19:40 +0800

    feat: page login

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import { IconfontGlobalStyle } from './statics/iconfont/iconfont'
 import store from './store'
 import Header from './common/header/index'
 import Home from './pages/home'
-import Detail from './pages/detail'
+import Detail from './pages/detail/loadable'
+import Login from './pages/login'
+import Write from './pages/write'
 
 class App extends Component {
   render () {
@@ -18,7 +20,9 @@ class App extends Component {
           <BrowserRouter>
             <Header />
             <Route path='/' exact component={Home}></Route>
+            <Route path='/login' exact component={Login}></Route>
             <Route path='/detail/:id' exact component={Detail}></Route>
+            <Route path='/write' exact component={Write}></Route>
           </BrowserRouter>
         </Provider>
       </>
--- src/common/header/index.js
+++ src/common/header/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { CSSTransition } from 'react-transition-group'
 import { actionCreators } from './store'
+import { actionCreators as loginActionCreators } from '../../pages/login/store'
 import { Link } from 'react-router-dom'
 import {
   HeaderWrapper,
@@ -22,7 +23,7 @@ import {
 class Header extends Component {
 
   render () {
-    const { focused, handleInputFocus, handleInputBlur, list } = this.props
+    const { focused, handleInputFocus, handleInputBlur, list, login, logout } = this.props
     return (
       <HeaderWrapper>
         <Link to="/">
@@ -31,7 +32,12 @@ class Header extends Component {
         <Nav>
           <NavItem className="left active">首页</NavItem>
           <NavItem className="left">下载App</NavItem>
-          <NavItem className="right">登录</NavItem>
+          {
+            login ?
+              <NavItem className="right" onClick={logout}>退出</NavItem> :
+              <Link to="/login"><NavItem className="right">登录</NavItem></Link>
+          }
+
           <NavItem className="right">
             <i className="iconfont">&#xe636;</i>
           </NavItem>
@@ -54,10 +60,12 @@ class Header extends Component {
           </SearchWrapper>
         </Nav>
         <Addition>
+        <Link to="/write">
           <Button className="writting">
             <i className="iconfont">&#xe615;</i>
             写文章
           </Button>
+          </Link>
           <Button className="reg">注册</Button>
         </Addition>
       </HeaderWrapper>
@@ -148,7 +156,8 @@ const mapStateToProps = (state) => {
     list: state.getIn(['header', 'list']),
     page: state.getIn(['header', 'page']),
     totalPage: state.getIn(['header', 'totalPage']),
-    mouseIn: state.getIn(['header', 'mouseIn'])
+    mouseIn: state.getIn(['header', 'mouseIn']),
+    login: state.getIn(['login', 'login'])
   }
 }
 
@@ -180,6 +189,9 @@ const mapDispatchToProps = (dispatch) => {
       } else {
         dispatch(actionCreators.changePage(1))
       }
+    },
+    logout () {
+      dispatch(loginActionCreators.logout())
     }
   }
 }
